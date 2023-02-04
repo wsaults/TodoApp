3d8b24f524c6701d882cee2087adc0a13bc95e85
Will Saults <wsaults@yahoo.com>
2023-02-04 06:30:40 -0600

    Calls store save method when saving

--- TodoEngine/TodoEngine/Models/TodoItem.swift
+++ TodoEngine/TodoEngine/Models/TodoItem.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct TodoItem {
+public struct TodoItem {
     let uuid: UUID
     let text: String?
     let createdAt: Date?
     let completedAt: Date?
+    
+    public init(uuid: UUID, text: String? = nil, createdAt: Date? = nil, completedAt: Date? = nil) {
+        self.uuid = uuid
+        self.text = text
+        self.createdAt = createdAt
+        self.completedAt = completedAt
+    }
 }
--- TodoEngine/TodoEngineTests/TodoCache/CachedTodosUseCaseTests.swift
+++ TodoEngine/TodoEngineTests/TodoCache/CachedTodosUseCaseTests.swift
@@ -5,16 +5,28 @@
 //  Created by Will Saults on 2/4/23.
 //
 
+import TodoEngine
 import XCTest
 
 class LocalTodoLoader {
+    private let store: TodoStore
+    
     init(store: TodoStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [TodoItem]) {
+        store.save()
     }
 }
 
 class TodoStore {
     var deleteCachedTodoCallCount = 0
+    var savedCachedTodoCallCount = 0
+    
+    func save() {
+        savedCachedTodoCallCount += 1
+    }
 }
 
 class CachedTodosUseCaseTests: XCTestCase {
@@ -25,4 +37,22 @@ class CachedTodosUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.deleteCachedTodoCallCount, 0)
     }
+    
+    func test_save() {
+        let store = TodoStore()
+        let sut = LocalTodoLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedTodoCallCount, 0)
+        XCTAssertEqual(store.savedCachedTodoCallCount, 1)
+    }
+    
+    // MARK: Helpers
+    
+    private func uniqueItem() -> TodoItem {
+        TodoItem(uuid: UUID(), text: "any", createdAt: anyDate, completedAt: anyDate)
+    }
+    
+    private let anyDate = Date()
 }
