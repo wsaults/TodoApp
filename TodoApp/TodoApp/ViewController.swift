abf06fefe76b946b9fcb8b7681c44bc04498c906
Will Saults <wsaults@yahoo.com>
2023-02-06 18:54:31 -0600

    Add empty TodoApp project

---
+++ TodoApp/TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Will Saults on 2/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
